f683ac273024399fb6d0239d891ef2e545a10ba6
ymeister <47071325+ymeister@users.noreply.github.com>
2026-03-17 00:07:59 +0900

    Fixup flake.nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-          project = pkgs.callPackage ./default.nix { inherit pkgs; };
-          overlay = pkgs.callPackage ./overlay.nix { inherit pkgs; };
-      in {
-        defaultPackage = project;
-        packages.overlay = overlay;
-      }
-    );
+  outputs = { self, nixpkgs, ... }:
+    let eachSystem = nixpkgs.lib.genAttrs
+          [ "x86_64-linux"
+            "aarch64-linux"
+          ];
+    in {
+      packages = eachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+            project = pkgs.callPackage ./default.nix { inherit pkgs; };
+            overlay = pkgs.callPackage ./overlay.nix { inherit pkgs; };
+        in {
+          default = project;
+          overlay = overlay;
+        }
+      );
+    };
 }
